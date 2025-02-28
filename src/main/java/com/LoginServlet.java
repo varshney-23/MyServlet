27be6ef0c4c5b5cb9d267dd1a20f3e3ce936bdb5
varshney-23 <ayushivarshney40@gmail.com>
2025-02-28 20:17:59 +0530

    password credentials and regex done

--- src/main/java/com/LoginServlet.java
+++ src/main/java/com/LoginServlet.java
@@ -25,14 +25,15 @@ public class LoginServlet extends HttpServlet{
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String nameRegex="^[A-Z]{1}[a-z]{3,}$";
+        String passRegex="(?=.[0-9])(?=.[A-Z])(?=.[a-z])(?=.[@#$%]).{8,20}";
+
         String user = req.getParameter("user");
         String pwd = req.getParameter("pwd");
 
 
         String userID = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
-
-        if((userID.equals(user) && password.equals(pwd)&&Pattern.matches(nameRegex,userID))) {
+        if((userID.equals(user) && password.equals(pwd)&&Pattern.matches(nameRegex,userID))&& Pattern.matches(passRegex,password)) {
             req.setAttribute("user",user);
             req.getRequestDispatcher("LoginSuccess.jsp").forward(req, resp);
         } else {
